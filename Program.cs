f81e66fdc93033d913e02efcf1e0be90f878dade
saif el_dein tarek <saifel.dein01010@gmail.com>
2025-02-24 15:31:45 +0200

    Implementation Mathemetical design screen core

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 ﻿
+using CalculatorProject.UI;
+
 namespace CalculatorProject
 {
     class Program
     {
         static void Main()
         {
+
+            MathematicalDesignScreen.MathematicalCalculatorScreen();
 
             Console.ReadKey();
 
---
+++ UI/MathematicalDesignScreen.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+
+namespace CalculatorProject.UI
+{
+    public class MathematicalDesignScreen
+    {
+
+        static private void ErrorMessage()
+        {
+
+            Console.Write("Wronge Commed/expression. Enter vaild Commed/expression >> ");
+
+        }
+        static private void HeaderScreen()
+        {
+
+            Console.WriteLine("\t\t\t\t    < Mathematical Calculator >\n\n");
+
+        }
+
+        static private void ExplainScreen()
+        {
+
+            Console.WriteLine("To perform Commend line screen write \"Commend\" without quote");
+            Console.WriteLine("To perform expression write your expression (Number1) (Operator) (Number2) Then press Enter\n");
+
+        }
+
+
+
+
+        static public void MathematicalCalculatorScreen()
+        {
+
+            HeaderScreen();
+            ExplainScreen();
+
+            Console.Write("  Enter \"Commend\" or Expression statement \n  >> ");
+
+        }
+
+    }
+}
+
